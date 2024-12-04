53c9c0bbf04f0497e2e80bd99e5945ff4d5c9be2
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-12-04 11:04:39 +0300

    added prettier

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import { Command } from "commander";
 const program = new Command();
 
 program
-  .name("my-cli")
-  .description("An example CLI built with TypeScript")
-  .version("1.0.0");
+	.name("my-cli")
+	.description("An example CLI built with TypeScript")
+	.version("1.0.0");
 
 program
-  .command("greet <name>")
-  .description("Greet a person")
-  .option("-u, --uppercase", "Convert the name to uppercase")
-  .action((name: string, options: { uppercase: boolean }) => {
-    const greeting = options.uppercase
-      ? `Hello, ${name.toUpperCase()}!`
-      : `Hello, ${name}!`;
-    console.log(greeting);
-  });
+	.command("greet <name>")
+	.description("Greet a person")
+	.option("-u, --uppercase", "Convert the name to uppercase")
+	.action((name: string, options: { uppercase: boolean }) => {
+		const greeting = options.uppercase
+			? `Hello, ${name.toUpperCase()}!`
+			: `Hello, ${name}!`;
+		console.log(greeting);
+	});
 
 program.parse(process.argv);
